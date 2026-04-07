4f18470313d0fe147dbfaf6a31e3ecfee1b61b07
SwathiM5 <swathimaganti@gmail.com>
2026-04-06 21:45:59 -0400

    Added changes to Dungeon Adventure

--- Text_File_Creator/file_creator.py
+++ Text_File_Creator/file_creator.py
@@ -1,6 +1,6 @@
 name = input("What's your name? \n")
 color = input("Favorite color: ")
-with open('example.txt', 'w') as file:
+with open('example1.txt', 'w') as file:
     file.write(f"{name} created this file")
-with open('example.txt', 'w') as file:
+with open('example1.txt', 'w') as file:
     file.write(f"{color} created this file")
